d35f098e47fa565e5cb1f7e9b8dec8ef1b98c83e
samyam-aryal <samyam.aryal@fusemachines.com>
2023-08-05 21:50:43 +0545

    Linted the code using pylint

--- Assignment 2/exceptions_3.py
+++ Assignment 2/exceptions_3.py
@@ -1,12 +1,29 @@
+"""
+Math Operations Module
+
+This module contains a function 'division' to perform division of two numbers.
+"""
+
 def division(num1, num2):
+    """
+    Return the quotient when num1 is divided by num2.
+
+    Args:
+        num1 (int): Dividend.
+        num2 (int): Divisor.
+
+    Returns:
+        int: Quotient of num1 divided by num2.
+    """
     try:
-        quot = int(num1)/int(num2)
+        quot = int(num1) / int(num2)
     except ZeroDivisionError:
         print("Zero Division Error: can't divide by 0")
+        return None
     except ValueError:
         print("Value Error.")
-    else:
-        return quot
+        return None
+    return quot
 
 print("Try 1")
 print("\n", division(10, 2))
