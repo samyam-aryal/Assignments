2dccc59a06bf9b980386ffb73fc3380696ef8e41
samyam-aryal <samyam.aryal@fusemachines.com>
2023-08-05 21:54:57 +0545

    Linted the code using Pylint.

--- Assignment 2/exceptions_4.py
+++ Assignment 2/exceptions_4.py
@@ -1,16 +1,38 @@
-class InvalidAgeError(Exception):
-    pass
+"""
+exceptions_4.py
 
-def get_age(age):
-    if int(age) < 0 or int(age) > 120:
+This module demonstrates the use of custom exceptions to handle invalid age values.
+"""
+
+
+class InvalidAgeError(Exception):
+    """
+    Custom exception for invalid age values.
+    """
+
+def get_age(age_str):
+    """
+    Convert the input age string to an integer and check if it's within the valid range.
+
+    Args:
+        age_str (str): The age as a string.
+
+    Returns:
+        int: The valid age as an integer.
+
+    Raises:
+        InvalidAgeError: If the age is not within the valid range (0 to 120).
+    """
+    age = int(age_str)
+    if age < 0 or age > 120:
         raise InvalidAgeError("Age must be between 0 and 120.")
     return age
 
+if __name__ == "__main__":
+    age_input = input("Enter age: ")
 
-age = input("Enter age ")
-
-try:
-    age = get_age(age)
-    print("Age is:", age)
-except InvalidAgeError as e:
-    print(e)
+    try:
+        valid_age = get_age(age_input)
+        print("Age is:", valid_age)
+    except InvalidAgeError as e:
+        print(e)
